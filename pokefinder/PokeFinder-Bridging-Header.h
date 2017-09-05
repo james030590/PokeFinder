612070dc357f3efd9c7cb253849c7e0db72a546e
James McLean <james@Jamess-iMac.local>
2017-09-05 15:45:25 +0100

    try again, final project finished

--- pokefinder/PokeFinder-Bridging-Header.h
+++ pokefinder/PokeFinder-Bridging-Header.h
@@ -6,8 +6,4 @@
 //  Copyright © 2017 James McLean. All rights reserved.
 //
 
-#ifndef PokeFinder_Bridging_Header_h
-#define PokeFinder_Bridging_Header_h
-
-
-#endif /* PokeFinder_Bridging_Header_h */
+#import "GeoFire.h"
