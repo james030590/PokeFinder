2126f3c0bae5d095e23bec54aa1f9dd60e34f3f2
James McLean <james@Jamess-iMac.local>
2017-09-05 15:44:00 +0100

    first commit, project finished and working

---
+++ pokefinder/PokeAnnotation.swift
@@ -0,0 +1,9 @@
+//
+//  PokeAnnotation.swift
+//  pokefinder
+//
+//  Created by James McLean on 05/09/2017.
+//  Copyright © 2017 James McLean. All rights reserved.
+//
+
+import Foundation
---
+++ pokefinder/PokeFinder-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  PokeFinder-Bridging-Header.h
+//  pokefinder
+//
+//  Created by James McLean on 05/09/2017.
+//  Copyright © 2017 James McLean. All rights reserved.
+//
+
+#ifndef PokeFinder_Bridging_Header_h
+#define PokeFinder_Bridging_Header_h
+
+
+#endif /* PokeFinder_Bridging_Header_h */
